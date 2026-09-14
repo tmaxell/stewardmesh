3f842261c52727513f97246dd3c0bccde096a9e6
Tim Makhel <tmaxelhard@gmail.com>
2026-09-14 12:56:58 +0300

    test(intake): prove mapping over secured MCP

--- master-data-service/bootstrap-master-service/src/test/java/io/stewardmesh/masterdata/bootstrap/StewardMeshApplicationIT.java
+++ master-data-service/bootstrap-master-service/src/test/java/io/stewardmesh/masterdata/bootstrap/StewardMeshApplicationIT.java
@@ -140,7 +140,7 @@ class StewardMeshApplicationIT {
         assertNotNull(profileIntakeArtifact);
         assertNotNull(consumeReferenceDataEvent);
         assertNotNull(publishMasterDataEvents);
-        assertEquals(7, toolCallbacks.getToolCallbacks().length);
+        assertEquals(9, toolCallbacks.getToolCallbacks().length);
     }
 
     @Test
